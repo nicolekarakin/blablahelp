77ed955e37ef9c17a610337a3af43c81982490ab
nicolekarakin <nicole.karakin@gmail.com>
2022-08-17 09:27:53 +0200

    --setup

---
+++ blablahelp-backend/src/main/java/org/nnn4eu/hfische/blablahelp/blablahelpbackend/BlablahelpBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.hfische.blablahelp.blablahelpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablahelpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlablahelpBackendApplication.class, args);
+    }
+
+}
