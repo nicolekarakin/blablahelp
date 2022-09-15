1e951454d16e363f0667a181c3d52be7088047f8
nicolekarakin <nicole.karakin@gmail.com>
2022-09-15 15:13:31 +0200

    imports

--- blablahelp-backend/src/main/java/org/nnn4eu/hfische/blablahelp/blablahelpbackend/account/AccountRepo.java
+++ blablahelp-backend/src/main/java/org/nnn4eu/hfische/blablahelp/blablahelpbackend/account/AccountRepo.java
@@ -1,10 +1,8 @@
 package org.nnn4eu.hfische.blablahelp.blablahelpbackend.account;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.time.Instant;
 import java.util.List;
 
 public interface AccountRepo extends MongoRepository<Account, String> {
