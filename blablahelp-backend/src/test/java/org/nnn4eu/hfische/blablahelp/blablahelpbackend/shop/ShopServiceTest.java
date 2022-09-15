101bc2134fc3c4cd06f7aeaf5e583fbb0721e298
nicolekarakin <nicole.karakin@gmail.com>
2022-09-15 14:47:17 +0200

    new tests for ShopServiceTest

--- blablahelp-backend/src/test/java/org/nnn4eu/hfische/blablahelp/blablahelpbackend/shop/ShopServiceTest.java
+++ blablahelp-backend/src/test/java/org/nnn4eu/hfische/blablahelp/blablahelpbackend/shop/ShopServiceTest.java
@@ -1,13 +1,19 @@
 package org.nnn4eu.hfische.blablahelp.blablahelpbackend.shop;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.nnn4eu.hfische.blablahelp.blablahelpbackend.shared.model.Address;
+import org.nnn4eu.hfische.blablahelp.blablahelpbackend.userdata.web.CreateData;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,13 +35,28 @@ class ShopServiceTest {
 
     @Test
     void findAddressesByListId() {
-        String id="de-DE_aldi";
-        ShopList shopList=new ShopList(id, Set.of(new Address("Street","12345","City",null,null)));
+        String id = "de-DE_aldi";
+        ShopList shopList = new ShopList(id, Set.of(new Address("Street", "12345", "City", null, null)));
         Optional<ShopList> value = Optional.of(shopList);
         when(shopRepo.findById(id)).thenReturn(value);
-        Set<Address> actual=shopService.findAddressesByListId(id);
+        Set<Address> actual = shopService.findAddressesByListId(id);
         assertThat(actual).isEqualTo(shopList.getAddresses());
     }
 
+    @Test
+    void addAddresses() throws JsonProcessingException {
+        String id = "de-DE_aldi";
+        Address adr1 = CreateData.createAddressWithLocNoth();
+        Address adr2 = CreateData.createAddressWithLocSouth();
+        Set<Address> mset = new HashSet<>(Arrays.asList(adr1, adr2));
+        ShopList shopList = new ShopList(id, mset);
+        Set<Address> addresses = new HashSet<>(mset);
+
+        when(shopRepo.findById(id)).thenReturn(Optional.of(shopList));
+        when(shopRepo.save(any(ShopList.class))).thenReturn(shopList);
+
+        ShopList actual = shopService.addAddresses(id, addresses);
+        Assertions.assertEquals(actual.getAddresses().size(), shopList.getAddresses().size());
+    }
 
 }
